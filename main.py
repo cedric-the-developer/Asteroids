dadf6f6281f4250090bb58527f79b020a7b78517
cedric-the-developer <123194290+cedric-the-developer@users.noreply.github.com>
2024-09-26 15:09:59 +0200

    Updated main.py so users can close game.

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill(0)
         pygame.display.flip()
 
